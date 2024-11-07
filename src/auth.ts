0d21d0852e54634b78682a5cf69b44808d2ec160
LovelessCodes <hello@loveless.codes>
2024-11-07 10:05:59 +0100

    feat: add more social providers

--- src/auth.ts
+++ src/auth.ts
@@ -3,18 +3,31 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db"; // your drizzle instance
 import * as authSchema from "./db/auth-schema";
 
+const providers = ["discord", "google", "github", "microsoft", "twitch"];
+
+const configuredProviders = providers.reduce<
+	Record<string, { clientId: string; clientSecret: string }>
+>((acc, provider) => {
+	const id = process.env[`${provider.toUpperCase()}_CLIENT_ID`];
+	const secret = process.env[`${provider.toUpperCase()}_CLIENT_SECRET`];
+	if (id && id.length > 0 && secret && secret.length > 0) {
+		acc[provider] = { clientId: id, clientSecret: secret };
+	}
+	return acc;
+}, {});
+
 export const auth = betterAuth({
-    baseURL: process.env.BETTER_AUTH_URL!,
-    secret: process.env.BETTER_AUTH_SECRET!,
-	socialProviders: {
-		google: {
-			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-		},
+	baseURL: process.env.BETTER_AUTH_URL || "http://localhost:8558",
+	secret: process.env.BETTER_AUTH_SECRET || undefined,
+	socialProviders: configuredProviders,
+	emailAndPassword: {
+		enabled: true,
+		autoSignIn: true,
+		minPasswordLength: 8,
 	},
-    trustedOrigins: process.env.ALLOWED_ORIGINS?.split(',') || [],
-    database: drizzleAdapter(db, {
-        provider: "sqlite", // or "mysql", "sqlite"
-        schema: authSchema,
-    })
+	trustedOrigins: process.env.ALLOWED_ORIGINS?.split(",") || [],
+	database: drizzleAdapter(db, {
+		provider: "sqlite", // or "mysql", "sqlite"
+		schema: authSchema,
+	}),
 });
