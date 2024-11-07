d3b100d0e5810c36c4e86c1b5c1f0d9b4d736c0f
LovelessCodes <hello@loveless.codes>
2024-11-07 10:14:09 +0100

    fix: was unable to create local db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+const authToken = process.env.TURSO_DB_AUTH_TOKEN;
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/auth-schema.ts',
-  dialect: 'turso',
+  dialect: authToken ? 'turso' : 'sqlite',
   dbCredentials: {
     url: process.env.TURSO_DB_URL || 'file:dev.db',
     authToken: process.env.TURSO_DB_AUTH_TOKEN,
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/libsql";
+import * as authSchema from "./auth-schema";
 
 // You can specify any property from the libsql connection options
 export const db = drizzle({
