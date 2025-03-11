88f8ea839ae4d89e3849933b9a84e999c89d72e6
LovelessCodes <hello@loveless.codes>
2025-03-11 09:29:09 +0000

    feat(socials): added all socials that are available

--- src/auth.ts
+++ src/auth.ts
@@ -4,21 +4,53 @@ import { db } from "./db";
 import * as authSchema from "./db/auth-schema";
 
 const providers = [
+	"apple",
 	"discord",
-	"google",
+	"dropbox",
+	"facebook",
 	"github",
-	"microsoft",
-	"twitch",
 	"gitlab",
+	"google",
+	"linkedin",
+	"microsoft",
+	"reddit",
+	"roblox",
+	"spotify",
+	"tiktok",
+	"twitch",
+	"vk",
+	"x",
 ];
 
 export const configuredProviders = providers.reduce<
-	Record<string, { clientId: string; clientSecret: string }>
+	Record<string, { clientId: string; clientSecret: string, appBundleIdentifier?: string, tenantId?: string, requireSelectAccount?: boolean, clientKey?: string, issuer?: string }>
 >((acc, provider) => {
 	const id = process.env[`${provider.toUpperCase()}_CLIENT_ID`];
 	const secret = process.env[`${provider.toUpperCase()}_CLIENT_SECRET`];
 	if (id && id.length > 0 && secret && secret.length > 0) {
 		acc[provider] = { clientId: id, clientSecret: secret };
+	}
+	if (provider === "apple") {
+		const bundleId = process.env[`${provider.toUpperCase()}_APP_BUNDLE_IDENTIFIER`];
+		if (bundleId && bundleId.length > 0) {
+			acc[provider].appBundleIdentifier = bundleId;
+		}
+	}
+	if (provider === "gitlab") {
+		const issuer = process.env[`${provider.toUpperCase()}_ISSUER`];
+		if (issuer && issuer.length > 0) {
+			acc[provider].issuer = issuer;
+		}
+	}
+	if (provider === "microsoft") {
+		acc[provider].tenantId = "common";
+		acc[provider].requireSelectAccount = true;
+	}
+	if (provider === "tiktok") {
+		const key = process.env[`${provider.toUpperCase()}_CLIENT_KEY`];
+		if (key && key.length > 0) {
+			acc[provider].clientKey = key;
+		}
 	}
 	return acc;
 }, {});
